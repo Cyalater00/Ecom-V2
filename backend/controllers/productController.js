8b32df1fd17ebc4a11dc275e9c528eae221d401b
“JACHIMIKE <Jachimikeonwuka@gmail.comgit config --global user.name  JACHIMIKE ONWUKAgit config --global user.email Jachimikeonwuka@gmail.com>
2023-07-17 08:20:45 -0400

    updates

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+//@desc Fetch all products
+//@route GET/api/products
+//access is Public
+const getProducts = asyncHandler (async (req, res) => { 
+    const products = await Product.find({});
+    res.json(products);
+});
+
+
+//code below fetches a products
+// code below gets api/products/:id
+//the access is  Public
+const getProductsById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {getProducts, getProductsById};
